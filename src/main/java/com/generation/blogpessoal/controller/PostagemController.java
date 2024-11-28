ed5d8c8ec458b8d02f2ce44d8e06ac567e7f1d3e
AlvessGS <gabriels.alvess25@gmail.com>
2024-11-28 20:35:58 -0300

    primeiro commit

---
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,60 @@
+package com.generation.blogpessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.model.Postagens;
+import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagens>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagens> getById(@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagens>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagens> Post(@Valid @RequestBody Postagens postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	public ResponseEntity<Postagens> put(@Valid @RequestBody Postagens postagem) {
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/generation/blogpessoal/model/Postagens.java
@@ -0,0 +1,59 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity /*dando referencia para a tabela*/
+@Table(name = "tb_postagens") /*Vai criar uma tabela no banco de dados*/
+public class Postagens {
+	
+	@Id /*Declarando a chave primária*/
+	@GeneratedValue(strategy = GenerationType.IDENTITY) /*Auto incremento no banco de dados*/
+	private Long id;
+	
+	@NotBlank /*Seria o not null do banco de dados, que precisa ser preenchido, e não aceita espaços*/
+	@Size(min = 5 , max = 100) /*Determinada a quantidade de caracteres, sendo o minimo ou máximo*/
+	private String titulo;
+	
+	@NotBlank /*Seria o not null do banco de dados, que precisa ser preenchido, e não aceita espaços*/
+	@Size(min = 10 , max = 1000) /*Determinada a quantidade de caracteres, sendo o minimo ou máximo*/
+	private String texto;
+	
+	@UpdateTimestamp /*Atualiza em tempo real*/
+	private LocalDateTime data;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+}
